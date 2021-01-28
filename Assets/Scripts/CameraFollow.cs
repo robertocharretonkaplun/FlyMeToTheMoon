0145db8fe4c1495d53b57c56c5de11d5a9be304b
Charreton <rcharretonk@gmail.com>
2021-01-28 00:35:19 -0600

    charre - Concept

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+  [Header("Camera Attributes")]
+  public GameObject target;
+  public Vector3 offset;
+  [Range(1, 10)]
+  public float smoothFactor;
+  private void FixedUpdate() {
+    follow();
+  }
+
+  void follow()  {
+    Vector3 targetPos = target.transform.position + offset;
+    Vector3 smoothPos = Vector3.Lerp(transform.position, targetPos, smoothFactor * Time.fixedDeltaTime);
+    transform.position = smoothPos;
+
+  }
+}
