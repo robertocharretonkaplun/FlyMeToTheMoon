7ee73af78b94e2b37dac43f74c0352cdafb9ceaf
Juan Bosco Sánchez Vargas Mora <JuanBoSVM@gmail.com>
2021-01-29 15:39:35 -0600

    Juan - Main Men
    Added the main menu of the game. Fixed the player's animations

---
+++ Assets/Scripts/SpriteFloat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFloat : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public Rigidbody2D rigidBody;
+    Vector2 movement;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = -1;
+        movement.y = -1;
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y);
+        rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rigidBody.transform.Rotate(0, 0, 3, Space.Self);
+    }
+}
