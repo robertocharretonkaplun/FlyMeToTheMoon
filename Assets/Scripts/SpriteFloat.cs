76e1d71c779a9dbbca33b0efd707cfa1dfed41c4
Juan Bosco Sánchez Vargas Mora <JuanBoSVM@gmail.com>
2021-01-29 19:37:42 -0600

    Juan - Update
    Integrated Kirito's oxygen bar into the UI, added screen wrap (kinda broken), and updated the floor texture

--- Assets/Scripts/SpriteFloat.cs
+++ Assets/Scripts/SpriteFloat.cs
@@ -9,9 +9,63 @@ public class SpriteFloat : MonoBehaviour
     public Rigidbody2D rigidBody;
     Vector2 movement;
 
+    private Renderer[] renderers;
+    private bool isWrappingX = false;
+    private bool isWrappingY = false;
+
+    bool CheckRenderers()
+    {
+        foreach (Renderer renderer in renderers)
+        {
+            if (renderer.isVisible)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void ScreenWrap()
+    {
+        var isVisible = CheckRenderers();
+
+        if (isVisible)
+        {
+            isWrappingX = false;
+            isWrappingY = false;
+            return;
+        }
+
+        if (isWrappingX && isWrappingY)
+        {
+            return;
+        }
+
+        var cam = Camera.main;
+        var viewportPosition = cam.WorldToViewportPoint(rigidBody.position);
+        var newPosition = rigidBody.position;
+        
+        if (!isWrappingX && (viewportPosition.x > 1 || viewportPosition.x < 0))
+        {
+            newPosition.x = -newPosition.x;
+            isWrappingX = true;
+        }
+
+        if (!isWrappingY && (viewportPosition.y > 1 || viewportPosition.y < 0))
+        {
+            newPosition.y = -newPosition.y;
+
+            isWrappingY = true;
+        }
+
+        transform.position = newPosition;
+    }
+
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();        
+        rigidBody = GetComponent<Rigidbody2D>();
+        renderers = GetComponentsInChildren<Renderer>(); 
     }
 
     // Update is called once per frame
@@ -26,5 +80,7 @@ public class SpriteFloat : MonoBehaviour
         rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y);
         rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
         rigidBody.transform.Rotate(0, 0, 3, Space.Self);
-    }
+
+        ScreenWrap();
+    }    
 }
